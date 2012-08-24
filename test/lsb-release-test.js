3713d4bd1b1c090ed583f7c4908e51675648c9d7
Maciej Małecki <maciej.malecki@notimplemented.org>
2012-08-24 15:13:21 +0200

    [test] Add tests

---
+++ test/lsb-release-test.js
@@ -0,0 +1,24 @@
+var fs = require('fs'),
+    path = require('path'),
+    assert = require('assert'),
+    child_process = require('child_process'),
+    assertCalled = require('assert-called'),
+    lsbRelease = require('../');
+
+child_process.exec = function () {
+  var cb = arguments[arguments.length - 1];
+  process.nextTick(function () {
+    cb(null, fs.readFileSync(path.join(__dirname, 'fixtures', 'fedora')), new Buffer(0));
+  });
+};
+
+lsbRelease(assertCalled(function (err, data) {
+  assert(!err);
+  assert.deepEqual(data, {
+    "lsbVersion": ":core-4.0-amd64:core-4.0-noarch",
+    "distributorID": "Fedora",
+    "description": "Fedora release 14 (Laughlin)",
+    "release": "14",
+    "codename":"Laughlin"
+  });
+}));
